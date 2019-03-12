07ce1529074e31772d26ef8d1b6ee6ddc796aa00
Yingqi Chen <chanwk18@hotmail.com>
2019-03-12 05:50:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@
 def position_taken? (board, index)
   if (board[index]=="X")||(board[index]=="O")
     true
-  else false
+#  the "else false" doesn't necessarily have to be put here because without it, the method will return "nil", which is considered to be falsey too
   end
 end
 
