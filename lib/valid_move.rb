73de12816d02a600a206a6687ccb29aaadfb4b6f
Yingqi Chen <chanwk18@hotmail.com>
2019-03-12 05:55:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,8 +10,8 @@ def position_taken? (board, index)
 end
 
 def valid_move?(board, index)
-  if (index.between?(0,8))
-    if (!position_taken?(board,index))
+  if (index.between?(1,9))
+    if (!position_taken?(board,index-1))
       return true
     end
 
